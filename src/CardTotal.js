744c2cb0e453903127229ef56219207e0850b4cc
MateusLouzada <mateuslouzada01@gmail.com>
2022-02-12 11:54:43 -0300

    criado o mecanismo de aparecer todos os cards de acordo com o periodo colocado

--- src/CardTotal.js
+++ src/CardTotal.js
@@ -1,37 +1,49 @@
 import React from 'react';
-import { Box, Button, Card, Typography, CardContent } from '@mui/material';
+import { Box, Card, Typography, CardContent, Fade } from '@mui/material';
 
 export default function CardTotal({ montante, juros, periodo }) {
 
-    const card = (
-        <React.Fragment>
-          <CardContent>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-              Mês
-            </Typography>
-            <Typography>
-            {montante}
-            </Typography>
-            <Typography>
-            {juros}
-            </Typography>
-            <Typography>
-            {periodo}
-            </Typography>
-          </CardContent>
-        </React.Fragment>
-      );
+  let cards = [];
 
-    return (
-        <Box sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            width: '100%',
-            height: '40%'
-        }}>
-            <Box sx={{ display: 'flex', alignSelf: "flex-start", width: "50%", justifyContent: "space-evenly" }}>
-                <Card variant="outlined">{card}</Card>
-            </Box>
-        </Box>
-    );
+  const card = () => {
+    for (let index = 0; index < periodo; index++) {
+      const array = <React.Fragment>
+        <Fade in={true}>
+          <Card sx={{ display: 'flex', justifyContent: 'space-evenly' }} variant="outlined">
+            <CardContent>
+              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                Mês
+              </Typography>
+              <Typography>
+                {montante}
+              </Typography>
+              <Typography>
+                {juros}
+              </Typography>
+              <Typography>
+                {periodo}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Fade>
+      </React.Fragment>;
+      cards.push(array)
+    }
+
+  };
+
+  card();
+
+  return (
+    <Box sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      width: '100%',
+      height: '40%'
+    }}>
+      <Box sx={{ display: 'flex', flexWrap: 'wrap', alignSelf: "flex-start", width: "50%", justifyContent: "space-evenly" }}>
+        {cards}
+      </Box>
+    </Box>
+  );
 }
